c838b5a9c2650cbff996202a2c24d51363708e1e
namphph <nampp@foxcode.org>
2025-08-29 08:56:22 +0700

    fix use safe dragging padding

--- app/src/main/java/com/amazon/democapability/MainActivity.kt
+++ app/src/main/java/com/amazon/democapability/MainActivity.kt
@@ -4,26 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.amazon.democapability.ui.theme.DemoCapabilityTheme
 
+// safeDrawingPadding() ->
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DemoCapabilityTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Yellow)
+                    .safeDrawingPadding()) {
+                    Greeting(name = "Nam")
                 }
             }
         }
